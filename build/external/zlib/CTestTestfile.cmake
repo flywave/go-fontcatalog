d67043ca1b3c55540471e02cf3da08583da93db4
aninggo <aninggo@gmail.com>
2021-08-23 18:27:45 +0800

    update

--- build/external/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aninggo/work/saas/go-fontcatalog/external/zlib
-# Build directory: /home/aninggo/work/saas/go-fontcatalog/build/external/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
